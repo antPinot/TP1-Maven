07745160ada5b719c18b5a6279d556cc3e474937
Anthony Pinot <pinot.anthony@gmail.com>
2022-10-24 14:38:26 +0200

    TP 3

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,16 +1,21 @@
 package dev;
 
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        String asciiHello = FigletFont.convertOneLine("Hello World!" );
-        System.out.println(asciiHello);
-    }
+public class App {
+	public static void main(String[] args) {
+		String titre = ResourceBundle.getBundle("application").getString("titre");
+		String asciiHello = FigletFont.convertOneLine(titre);
+		System.out.println(asciiHello);
+
+		String environnement = ResourceBundle.getBundle("application").getString("environnement");
+		System.out.println("Environnement : " + environnement);
+
+	}
 }
